8147fc8608acfe75482cf6b703ccee72dd2a7e35
Nicholas Segger <painn5@yahoo.com>
2020-11-04 03:48:34 -0300

    refactor: Use JNA to check if process is running.

--- src/main/java/iamn5/edpn/app/utils/Processes.java
+++ src/main/java/iamn5/edpn/app/utils/Processes.java
@@ -1,37 +1,29 @@
 package iamn5.edpn.app.utils;
 
-import iamn5.edpn.framework.Logger;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Scanner;
+import com.sun.jna.Native;
+import com.sun.jna.platform.win32.*;
+import com.sun.jna.win32.W32APIOptions;
 
 public class Processes {
-
     public static boolean isEDRunning() {
-        try {
-            return isProcessRunning("EliteDangerous");
-        } catch (IOException e) {
-            Logger.error(e.getMessage());
+        return isProcessRunning("EliteDangerous");
+    }
+
+    public static boolean isProcessRunning(String processName) {
+        Kernel32 kernel32 = Native.load(Kernel32.class, W32APIOptions.UNICODE_OPTIONS);
+
+        Tlhelp32.PROCESSENTRY32.ByReference pEntry = new Tlhelp32.PROCESSENTRY32.ByReference();
+        WinNT.HANDLE hSnap = kernel32.CreateToolhelp32Snapshot(Tlhelp32.TH32CS_SNAPPROCESS, new WinDef.DWORD(0));
+
+        if (kernel32.Process32First(hSnap, pEntry)) {
+            do {
+                if (String.valueOf(pEntry.szExeFile).contains(processName)) {
+                    return true;
+                }
+
+            } while (kernel32.Process32Next(hSnap, pEntry));
         }
 
         return false;
-    }
-
-    public static boolean isProcessRunning(String processName) throws IOException {
-        ProcessBuilder processBuilder = new ProcessBuilder("tasklist.exe");
-        Process process = processBuilder.start();
-        String taskList = toString(process.getInputStream());
-
-        return taskList.contains(processName);
-    }
-
-    private static String toString(InputStream inputStream)
-    {
-        Scanner scanner = new Scanner(inputStream, "UTF-8").useDelimiter("\\A");
-        String string = scanner.hasNext() ? scanner.next() : "";
-        scanner.close();
-
-        return string;
     }
 }
