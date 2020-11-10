b5975843cd0d0ef791c1305ef54f942e89f03806
Nicholas Segger <painn5@yahoo.com>
2020-11-10 17:03:54 -0300

    feat: Add RoundJTextField component

---
+++ src/main/java/iamn5/edpn/screens/common/RoundJTextField.java
@@ -0,0 +1,47 @@
+package iamn5.edpn.screens.common;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
+
+public class RoundJTextField extends JTextField {
+    private Shape shape;
+    private int radius;
+
+    public RoundJTextField() {
+        this(5);
+    }
+
+    public RoundJTextField(int radius) {
+        this.radius = radius;
+        setOpaque(false);
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    protected void paintComponent(Graphics g) {
+        g.setColor(getBackground());
+        g.fillRoundRect(0, 0, getWidth()-1, getHeight()-1, radius, radius);
+        super.paintComponent(g);
+    }
+
+    protected void paintBorder(Graphics g) {
+        g.setColor(getBackground());
+        g.drawRoundRect(0, 0, getWidth()-1, getHeight()-1, radius, radius);
+    }
+
+    public boolean contains(int x, int y) {
+        if (shape == null || !shape.getBounds().equals(getBounds())) {
+            shape = new RoundRectangle2D.Float(0, 0, getWidth()-1, getHeight()-1, radius, radius);
+        }
+
+        return shape.contains(x, y);
+    }
+}
+
